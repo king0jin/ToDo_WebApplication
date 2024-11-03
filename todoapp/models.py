92e7c94936005fe2e7853ed17b474e6bf2340eb9
king0jin <et0709@naver.com>
2024-11-03 16:53:15 +0900

    모델(테이블) 생성, 연동

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    userid = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    isdone= models.BooleanField(default=False)
+    untildate = models.DateField()
